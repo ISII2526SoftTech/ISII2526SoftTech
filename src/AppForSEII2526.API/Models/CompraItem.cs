17892476bbaa8bab55c9ca901b87c42039f13812
Manuel <manuel.castano1@alu.uclm.es>
2025-10-03 12:54:50 +0200

    9 crear clase comprar (#10)
    * he creado 3 nuevas clases, lo subo con la migración
    
    * he hecho las 3 clases sin migración

---
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -0,0 +1,32 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class CompraItem
+    {
+        // Propiedades básicas
+        public int Id { get; set; }
+        public int cantidad { get; set; }
+        public string descripcion { get; set; }
+        public int idCompra { get; set; }
+        public int idHerramienta { get; set; }
+        public decimal precio { get; set; }
+
+        // Relación: Un CompraItem pertenece a una Compra
+        public Comprar Compra { get; set; }
+
+        // Constructores
+        public CompraItem()
+        {
+        }
+
+        public CompraItem(int id, int cantidad, string descripcion, int idCompra,
+                         int idHerramienta, decimal precio)
+        {
+            Id = id;
+            this.cantidad = cantidad;
+            this.descripcion = descripcion;
+            this.idCompra = idCompra;
+            this.idHerramienta = idHerramienta;
+            this.precio = precio;
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/Comprar.cs
@@ -0,0 +1,43 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Comprar
+    {
+        public int Id { get; set; }
+        public string nombreCliente { get; set; }
+        public string apellidoCliente { get; set; }
+        public string correoElectronico { get; set; }
+        public string telefono { get; set; }
+        public string direccionEnvio { get; set; }
+        public DateTime fechaCompra { get; set; }
+        public decimal precioTotal { get; set; }
+
+
+        public TipoMetodoPago metodoPago { get; set; }
+
+
+        public virtual List<CompraItem> CompraItems { get; set; }
+
+
+        public Comprar()
+        {
+            CompraItems = new List<CompraItem>();
+        }
+
+        public Comprar(int id, string nombreCliente, string apellidoCliente, string correoElectronico,
+                      string telefono, string direccionEnvio, DateTime fechaCompra,
+                      decimal precioTotal, TipoMetodoPago metodoPago)
+        {
+            Id = id;
+            this.nombreCliente = nombreCliente;
+            this.apellidoCliente = apellidoCliente;
+            this.correoElectronico = correoElectronico;
+            this.telefono = telefono;
+            this.direccionEnvio = direccionEnvio;
+            this.fechaCompra = fechaCompra;
+            this.precioTotal = precioTotal;
+            this.metodoPago = metodoPago;
+            CompraItems = new List<CompraItem>();
+
+        }
+    }
+}
