3f7c197d1491f71cc4fec8e843dfda987de7d376
SERGIO SÁNCHEZ VALERO <Sergio.Sanchez50@alu.uclm.es>
2025-10-10 10:43:51 +0200

    Vale, he eliminado la base de datos que funciona correctamente. Subo otras migraciones nuevas, elimino las anteriores.

--- src/AppForSEII2526.API/Migrations/20251010084141_CreateIdentitySchema.Designer.cs
+++ src/AppForSEII2526.API/Migrations/20251010084141_CreateIdentitySchema.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppForSEII2526.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251010083517_CreateIdentitySchema")]
+    [Migration("20251010084141_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
         /// <inheritdoc />
