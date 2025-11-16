3643a4619297208b72d87124d446b34e8749a97e
MANUEL CASTAÑO CUERDA <Manuel.Castano1@alu.uclm.es>
2025-11-16 22:18:30 +0100

    ampliacion del contenido de los dtos de compra

--- src/AppForSEII2526.API/DTOs/ComprarDTOs/ComprarDetailDTO.cs
+++ src/AppForSEII2526.API/DTOs/ComprarDTOs/ComprarDetailDTO.cs
@@ -1,24 +1,21 @@
-﻿namespace AppForSEII2526.API.DTOs.ComprarDTOs
-{
-    public class ComprarDetailDTO
-    {
-        private string direccionEnvio;
-        private IList<CompraItem> comprarItem;
-        private List<ComprarItemDTO> comprarItemDTOs;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
+namespace AppForSEII2526.API.DTOs.ComprarDTOs
+{
+    public class ComprarDetailDTO : IEquatable<ComprarDetailDTO>
+    {
         public ComprarDetailDTO(string nombreCliente, string apellidoCLiente, string direccion, DateTime fechaCompra, decimal precioTotal, IList<ComprarItemDTO> comprarItem)
         {
-
             NombreCliente = nombreCliente;
             ApellidoCLiente = apellidoCLiente;
             Direccion = direccion;
             PrecioTotal = precioTotal;
             FechaCompra = fechaCompra;
-            PrecioTotal = precioTotal;
             ComprarItem = comprarItem;
-
         }
-
 
         [Display(Name = "Nombre")]
         public string NombreCliente { get; set; }
@@ -31,15 +28,46 @@
         public DateTime FechaCompra { get; set; }
         public IList<ComprarItemDTO> ComprarItem { get; set; }
 
-        public override bool Equals(object? obj)
+        public override bool Equals(object? obj) => Equals(obj as ComprarDetailDTO);
+
+        public bool Equals(ComprarDetailDTO? other)
         {
-            return obj is ComprarDetailDTO dTO &&
-                   NombreCliente == dTO.NombreCliente &&
-                   ApellidoCLiente == dTO.ApellidoCLiente &&
-                   Direccion == dTO.Direccion &&
-                   PrecioTotal == dTO.PrecioTotal &&
-                   FechaCompra == dTO.FechaCompra &&
-                   EqualityComparer<IList<ComprarItemDTO>>.Default.Equals(ComprarItem, dTO.ComprarItem);
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            // Comparación por propiedades escalares
+            var sameScalars =
+                NombreCliente == other.NombreCliente &&
+                ApellidoCLiente == other.ApellidoCLiente &&
+                Direccion == other.Direccion &&
+                PrecioTotal == other.PrecioTotal &&
+                FechaCompra == other.FechaCompra;
+
+            if (!sameScalars) return false;
+
+            // Comparación por contenido de la lista (SequenceEqual usa ComprarItemDTO.Equals)
+            if (ComprarItem == null && other.ComprarItem == null) return true;
+            if (ComprarItem == null || other.ComprarItem == null) return false;
+
+            return ComprarItem.SequenceEqual(other.ComprarItem);
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = new HashCode();
+            hash.Add(NombreCliente);
+            hash.Add(ApellidoCLiente);
+            hash.Add(Direccion);
+            hash.Add(PrecioTotal);
+            hash.Add(FechaCompra);
+
+            if (ComprarItem != null)
+            {
+                foreach (var item in ComprarItem)
+                    hash.Add(item);
+            }
+
+            return hash.ToHashCode();
         }
     }
 }
