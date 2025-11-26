52827ed2322a490c46a32d561b04ddaac419bc0f
Sergio Sánchez Valero <153449082+Gambbon@users.noreply.github.com>
2025-11-26 10:54:39 +0100

    110 crear iu de getdetails cu oferta (#115)
    * He hecho mi state Container de CU oferta
    
    * Modificando el pipeline de azure
    
    Voy a probar primero en mi rama y si va lo meto en development
    
    * He hecho la IU de get Details CU oferta
    
    * Ya lo he probado y creo que funciona
    
    Lo añadire en mi rama y luego mergeare a development todo
    
    * He retocado el details porque lo hice un poco a mi manera, he mirado movies y ahora tiene mejor pinta.

---
+++ src/AppForSEII2526.Web/OfertaStateContainer.cs
@@ -0,0 +1,50 @@
+﻿using AppForSEII2526.Web.API;
+using Humanizer.DateTimeHumanizeStrategy;
+
+namespace AppForSEII2526.Web
+{
+    public class OfertaStateContainer
+    {
+        public OfertaForCreateDTO Oferta { get; set; } = new OfertaForCreateDTO()
+        {
+            OfertaItems = new List<OfertaItemDTO>()
+        };
+
+        public event Action? OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddOfertaItem(OfertaItemDTO herramienta)
+        {
+            if(!Oferta.OfertaItems.Any(oi => oi.HerramientaId == herramienta.HerramientaId))
+            {
+                Oferta.OfertaItems.Add(new OfertaItemDTO()
+                {
+                    HerramientaId = herramienta.HerramientaId,                    
+                    Porcentaje = herramienta.Porcentaje,
+                    PrecioOriginal = herramienta.PrecioOriginal,
+                    PrecioFinal = herramienta.PrecioFinal
+                });
+            }           
+        }
+
+        public void RemoveOfertaItem(OfertaItemDTO herramienta)
+        {
+            var itemToRemove = Oferta.OfertaItems.FirstOrDefault(oi => oi.HerramientaId == herramienta.HerramientaId);
+            if (itemToRemove != null)
+            {
+                Oferta.OfertaItems.Remove(itemToRemove);
+            }
+        }
+        public void ClearOfertaItems()
+        {
+            Oferta.OfertaItems.Clear();
+        }
+        public void OfertaProcessed()
+        {
+            Oferta = new OfertaForCreateDTO()
+            {
+                OfertaItems = new List<OfertaItemDTO>()
+            };
+        }
+    }
+}
