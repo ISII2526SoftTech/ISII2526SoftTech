0dca22639a213d333e9aac43cc46b9f7150ebe87
B1lly05 <billalchal446@gmail.com>
2025-12-15 22:31:16 +0100

    Joder el details hermano, subo esto de backup ahora subo el word

---
+++ test/AppForSEII2526.UIT/CU_Reparacion/CreateReparacion_PO.cs
@@ -0,0 +1,86 @@
+﻿using AppForSEII2526.UIT.Shared;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
+using Xunit.Abstractions;
+
+namespace AppForSEII2526.UIT.CU_Reparacion
+{
+    public class CreateReparacion_PO : PageObject
+    {
+
+        private By inputNombre = By.Id("Nombre");
+        private By inputApellidos = By.Id("Apellido");
+        private By inputTelefono = By.Id("Telefono");
+        private By inputFechaEntrega = By.Id("FechaEntrega");
+        private By inputMetodoPago = By.Id("MetodoPago");
+        private By btnRegistrarReparacion = By.Id("Submit");
+        private By btnModificarHerramientas = By.Id("ModifyReparacion");
+        private By dialogOkButton = By.Id("Button_DialogOK");
+        private By tableOfReparacionItems = By.Id("TableOfReparacionItems");
+        public CreateReparacion_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void RellenarFormularioReparacion(string nombreC, string apellidosC, string telefono, DateTime fechaEntrega, string metodoPago)
+        {
+            //Nombre
+            WaitForBeingClickable(inputNombre);
+            _driver.FindElement(inputNombre).Clear();
+            _driver.FindElement(inputNombre).SendKeys(nombreC);
+
+            //Apellidos
+            WaitForBeingClickable(inputApellidos);
+            _driver.FindElement(inputApellidos).Clear();
+            _driver.FindElement(inputApellidos).SendKeys(apellidosC);
+
+            //Teléfono
+            WaitForBeingClickable(inputTelefono);
+            _driver.FindElement(inputTelefono).Clear();
+            _driver.FindElement(inputTelefono).SendKeys(telefono);
+
+            //Fecha
+            WaitForBeingClickable(inputFechaEntrega);
+            _driver.FindElement(inputFechaEntrega).SendKeys(fechaEntrega.ToString("dd/MM/yyyy"));
+
+            //Método de Pago
+            WaitForBeingClickable(inputMetodoPago);
+            var selectElement = new SelectElement(_driver.FindElement(inputMetodoPago));
+            selectElement.SelectByText(metodoPago);
+        }
+
+        public void RellenarDescripcionReparacion(string descripcion, int idHerramienta)
+        {
+            By descripcionInput = By.Id($"Descripcion_{idHerramienta}");
+            WaitForBeingClickable(descripcionInput);
+            _driver.FindElement(descripcionInput).Clear();
+            _driver.FindElement(descripcionInput).SendKeys(descripcion);
+        }
+
+        public void ClickRegistrarButton()
+        {
+            WaitForBeingClickable(btnRegistrarReparacion);
+            _driver.FindElement(btnRegistrarReparacion).Click();
+        }
+ 
+        public void ConfirmDialog()
+        {
+            WaitForBeingClickable(dialogOkButton);
+            _driver.FindElement(dialogOkButton).Click();
+        }
+
+        public void ClickModificarHerramientas()
+        {
+            WaitForBeingClickable(btnModificarHerramientas);
+            _driver.FindElement(btnModificarHerramientas).Click();
+        }
+
+        public bool CheckValidationError(string error)
+        {
+            return _driver.PageSource.Contains(error);
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/CU_Reparacion/GetDetailsReparacion_PO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using Xunit.Abstractions;
+using AppForSEII2526.UIT.Shared;
+using System.Collections.Generic;
+
+namespace AppForSEII2526.UIT.CU_Reparacion
+{
+    public class GetDetailsReparacion_PO : PageObject
+    {
+        public GetDetailsReparacion_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        private By tablaReparacionItemBy = By.Id("HerramientasEnReparacion");
+
+        public bool CheckDetallesReparacion(string nombreCompleto, DateTime fechaEntrega, DateTime fechaRecogida, string metodoPago, string totalCoste)
+        {
+            WaitForBeingVisible(By.Id("Name"));
+
+            bool result = true;
+
+            result &= _driver.FindElement(By.Id("Name")).Text.Contains(nombreCompleto);
+            result &= _driver.FindElement(By.Id("FechaEntrega")).Text.Contains(fechaEntrega.ToString("dd/MM/yyyy"));
+            result &= _driver.FindElement(By.Id("FechaRecogida")).Text.Contains(fechaRecogida.ToString("dd/MM/yyyy"));
+            result &= _driver.FindElement(By.Id("MetodoPago")).Text.Contains(metodoPago);
+            result &= _driver.FindElement(By.Id("TotalCoste")).Text.Contains(totalCoste);
+
+            return result;
+        }
+
+    }
+}
